4a8a5b43c189e59e643f6b60592ec0f6878f3462
John Patrick Paraon <patrickparaon041@gmail.com>
2024-09-25 21:41:31 +0800

    Resolved merge conflicts

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import dotenv from "dotenv";
 
 const app = express();
 const port = 3000;
+dotenv.config();
 
 const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "todolist",
-  password: "johnpatrick022205",
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
+
 db.connect();
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
